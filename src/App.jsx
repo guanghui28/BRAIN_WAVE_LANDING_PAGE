d339d2bae6f2b3262b708b211632871751122a1f
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-02 21:40:23 +0700

    reusable Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
 export default function App() {
-    return <div className="bg-red-500 w-20 h-20"></div>;
+    return (
+        <>
+            <h1 className="text-red-500">Hello world</h1>
+
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Button className="mt-10" href="#login">
+                    Login
+                </Button>
+            </div>
+
+            <ButtonGradient />
+        </>
+    );
 }
